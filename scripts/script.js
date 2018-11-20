3685bbee9ba65e9d8669bd8ef0fc9aa84cf4bd71
Raymondl <raymond@altvfx.com>
2018-11-20 17:55:41 +1100

    reduced to 2d kelp

--- scripts/script.js
+++ scripts/script.js
@@ -20,10 +20,12 @@ var fd = Scene.root.child('Device').child('Camera').child('Focal Distance');
 var ui = fd.child('OutcomeMOdal');
 var textbox = ui.child('WinningModal').child('rectangle0').child('Box');
 
-const vase = Scene.root.child('TerracottaVase');
+const vase = fd.child('Rays').child('TerracottaVase');
+const uivase = fd.child('Rays').child('TerracottaVase0');
+
 const winModal = fd.child('OutcomeMOdal').child('WinningModal');
 const loseModal = fd.child('OutcomeMOdal').child('LosingModal');
-const closeWinModal = winModal.child('Close');
+const closeWinModal = winModal.child('rectangle0').child('Close');
 const closeLoseModal = loseModal.child('Close0');
 
 const tapChunks = [Scene.root.child('vase_pieces').child('model_GRP').child('TAP_CHUNK_1'), Scene.root.child('vase_pieces').child('model_GRP').child('TAP_CHUNK_2'), Scene.root.child('vase_pieces').child('model_GRP').child('TAP_CHUNK_3')];
@@ -39,7 +41,8 @@ const spawnRadius = 10;
 const floatSpeed = 2;
 var winLose = [fd.child('OutcomeMOdal').child('WinningModal'), fd.child('OutcomeMOdal').child('LosingModal')];
 var time = Reactive.val(0);
-
+var piecesCollected = [false, false, false];
+var allTrue = false;
 
 function init(){
 	animateFloating(tapChunks[0], 10, 2000);
@@ -103,14 +106,14 @@ function randomWinLose(){
 
 function animateFloating(obj, deltaY, ms){
 	//grab init pos
-	//var y0 = obj.transform.y.lastValue
+	var y0 = getRandomInt(-50, 50);
 
 	// create time driver
 	// create time driver mirroring the animation
 	// loop 999 times
 	obj.animy_driver = Animation.timeDriver({durationMilliseconds: ms, loopCount: Infinity, mirror: true});  
 	// create sampler
-	obj.animy_sampler = Animation.samplers.easeInOutSine(0, 0 + deltaY);
+	obj.animy_sampler = Animation.samplers.easeInOutSine(y0, y0 + deltaY);
   
 	// bind the animation to the object's property passing the driver and the sampler
 	obj.transform.y = Animation.animate(obj.animy_driver, obj.animy_sampler);
@@ -151,14 +154,52 @@ function fadeOut(obj, mat, ms, delay) {
 	obj.fadeout_driver.start();  }, delay);
 }
 
+function fadeOutPingPong(obj, mat, ms, delay) {
+	// start the animation
+	Time.setTimeout(	function (elapsedTime) {		// create the driver and sampler as properties of (inside) the object
+	obj.fadeout_driver = Animation.timeDriver({durationMilliseconds: ms, loopCount: Infinity, mirror: true});
+
+	obj.fadeout_sampler = Animation.samplers.easeInOutSine(1.0,0.0);
+
+	// it's good to reset the driver before the animation starts (in case you ran
+	// this animation before)
+	obj.fadeout_driver.reset();
+
+	// bind the opacity to the animation passing the driver and sampler
+	mat.opacity = Animation.animate(obj.fadeout_driver, obj.fadeout_sampler);
+	obj.fadeout_driver.start();  }, delay);
+}
+
+function testTrue(obj)
+{
+  for(var o in obj)
+      if(!obj[o]) return false;
+    
+  Diagnostics.log(obj);
+
+  vase.hidden = false;
+  uivase.hidden = true;
+  fadeOutPingPong(uiflasher, mat_vaseUIFlash, 800, 2000);
+  return true;
+}
+
+
+
 //TAP EVENTS
 //TAP ON PIECES
-TouchGestures.onTap(tapChunks[0]).subscribe(function (e) { animateScale(tapChunks[0], 1, 1000);  UIChunks[0].hidden = Reactive.val(true).delayBy({milliseconds: 1000}) ; fadeOut(flasher, mat_cloud, 400,0); fadeOut(uiflasher, mat_vaseUIFlash, 1000, 1000);});
-TouchGestures.onTap(tapChunks[1]).subscribe(function (e) { animateScale(tapChunks[1], 1, 1000);  UIChunks[1].hidden = Reactive.val(true).delayBy({milliseconds: 1000}) ; fadeOut(flasher, mat_cloud, 400,0); fadeOut(uiflasher, mat_vaseUIFlash, 1000, 1000);});
-TouchGestures.onTap(tapChunks[2]).subscribe(function (e) { animateScale(tapChunks[2], 1, 1000);  UIChunks[2].hidden = Reactive.val(true).delayBy({milliseconds: 1000}) ; fadeOut(flasher, mat_cloud, 400,0); fadeOut(uiflasher, mat_vaseUIFlash, 1000, 1000);});
+TouchGestures.onTap(tapChunks[0]).subscribe(function () { animateScale(tapChunks[0], 1, 1000);  UIChunks[0].hidden = Reactive.val(true).delayBy({milliseconds: 1000}) ; fadeOut(flasher, mat_cloud, 400,0); fadeOut(uiflasher, mat_vaseUIFlash, 1200, 1000); piecesCollected[0] = true; testTrue(piecesCollected);});
+TouchGestures.onTap(tapChunks[1]).subscribe(function () { animateScale(tapChunks[1], 1, 1000);  UIChunks[1].hidden = Reactive.val(true).delayBy({milliseconds: 1000}) ; fadeOut(flasher, mat_cloud, 400,0); fadeOut(uiflasher, mat_vaseUIFlash, 1200, 1000); piecesCollected[1] = true; testTrue(piecesCollected);});
+TouchGestures.onTap(tapChunks[2]).subscribe(function () { animateScale(tapChunks[2], 1, 1000);  UIChunks[2].hidden = Reactive.val(true).delayBy({milliseconds: 1000}) ; fadeOut(flasher, mat_cloud, 400,0); fadeOut(uiflasher, mat_vaseUIFlash, 1200, 1000); piecesCollected[2] = true; testTrue(piecesCollected);});
+
+
+//allTrue.monitor().subscribe(function (e){ if (e) {vase.hidden = false;
+//uivase.hidden = true;} } )
+
+TouchGestures.onTap(vase).subscribe(function () {	vase.hidden = true; 	randomWinLose(); uiflasher.hidden = true; });//spawnVase(); 	time = Reactive.val(-200); 	time = time.pin().add(Time.ms.sub(Time.ms.pin()).div(1000/8));		bindTime(); } );
+
+
 
 //TAP ON VASE
-TouchGestures.onTap(vase).subscribe(function () {	vase.hidden = true; 	spawnVase(); 	time = Reactive.val(-200); 	time = time.pin().add(Time.ms.sub(Time.ms.pin()).div(1000/8));	randomWinLose();	bindTime(); } );
 
 //CLOSE OUTCOME MODALS
 TouchGestures.onTap(closeWinModal).subscribe(function () { winModal.hidden = true; });
@@ -175,14 +216,15 @@ TouchGestures.onTap(textbox).subscribe(function(event) {
 // Another new method, getText(), returns the editable text as a StringSignal,
 // allowing you to monitor and attach an event subscription when the text
 // has been changed.
-// NativeUI.getText('Email').monitor().subscribe(function(event) {
-//     // Check if the text has indeed been changed
-//     if (event.newValue !== event.oldValue) {
-//         // Do actions 
-//         ui.child('Message').text = 'Welcome ' + event.newValue.split(' ')[0];
-//         ui.child('Message').hidden = false;
-//     }
-// });
+NativeUI.getText('Email').monitor().subscribe(function(event) {
+    // Check if the text has indeed been changed
+    if (event.newValue !== event.oldValue) {
+        // Do actions 
+        Diagnostics.log(event.newValue)
+        ui.child('Message').text = 'Welcome ' + event.newValue.split(' ')[0];
+        //ui.child('Message').hidden = false;
+    }
+});
 // vase.transform.y.monitor().subscribe(function(e) {
 // 	if (e >= 200) {
 // 		vase.hidden = true;
